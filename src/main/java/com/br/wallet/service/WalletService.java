36e570c5608f252760a6477e7365202fbccd3d5b
Antonio Marcos <antoniomarcos@unifor.br>
2025-02-01 08:41:54 -0300

    New feature

--- src/main/java/com/br/wallet/service/WalletService.java
+++ src/main/java/com/br/wallet/service/WalletService.java
@@ -5,6 +5,8 @@ import com.br.wallet.dto.response.WalletResponseDTO;
 import com.br.wallet.entity.Wallet;
 import com.br.wallet.exception.WalletNotFoundException;
 import com.br.wallet.repository.WalletRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,8 @@ import java.util.stream.Collectors;
 @Service
 public class WalletService {
 
+    private static final Logger logger = LoggerFactory.getLogger(WalletService.class);
+
     private final WalletRepository walletRepository;
 
     public WalletService(WalletRepository walletRepository) {
@@ -21,15 +25,26 @@ public class WalletService {
     }
 
     public List<WalletResponseDTO> getAllWallets(){
+        logger.info("Iniciando o método de busca de wallets");
+
         List<Wallet> wallets = walletRepository.findAll();
+
+        logger.info("Finalizando a buca de todas as wallets");
+
         return wallets.stream()
                 .map(wallet -> new WalletResponseDTO(wallet.getId(), wallet.getUserId(), wallet.getBalance()))
                 .collect(Collectors.toList());
     }
 
     public WalletResponseDTO getWalletById(Long id){
+
+        logger.info("Iniciando a busca por uma wallet com ID: {}", id);
+
         Wallet wallet = walletRepository.findById(id)
                 .orElseThrow(() -> new WalletNotFoundException("Wallet not found with ID: " + id));
+
+        logger.info("Finalizando a busca por uma wallet com ID: {}", wallet.getId());
+
         return new WalletResponseDTO(wallet.getId(), wallet.getUserId(), wallet.getBalance());
     }
 
